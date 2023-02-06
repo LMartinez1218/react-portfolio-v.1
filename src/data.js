0ce17c9b478f847427de1e51b52c4df0aa7be6ef
Lauren Martinez <Lauren.s.martinez@gmail.com>
2023-02-06 02:21:23 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+// data page where built arrays reside with data that is used on pages
+// projects, skills
+
+export const projects = [
+  {
+    title: "Server Blog",
+    subtitle: "MongoDB/POSTMAN",
+    description:
+      "Creating endpoints & server for a mock up server blog with full CRUD mobility",
+    image: "./postman.png",
+    link: "https://github.com/LMartinez1218/server-blog-pt-2.git",
+  },
+  {
+    title: "React Chat",
+    subtitle: "React",
+    description:
+      "Front-end designing a live chatroom with users and messages, routes in place to allow admin to create and delete chatrooms",
+    image: "./reactchat.jpg",
+    link: "https://github.com/burlingtoncodeacademy-students/react-chat-nov-22-lauren-james.git",
+  },
+  {
+    title: "Guess The Number",
+    subtitle: "Javascript",
+    description:
+      "A computer game in which the computer guesses a secret number and the user must let it know if it's too low/high or correct/incorrect. Developed with javascript the game continues on until it's guessed correctly and counts how many tries it took.",
+    image: "./gtm.jpg",
+    link: "https://github.com/LMartinez1218/GuessTHENum.git",
+  },
+  {
+    title: "Jeopardy Board",
+    subtitle: "Javascript/HTML/CSS",
+    description:
+      "Jeopardy board built with flippable card titles, javascript was used to filter questions and answers with each given category.",
+    image: "./jep.jpg",
+    link: "https://github.com/LMartinez1218/resubmission-jeopardy.git",
+  },
+];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React/Redux",
+  "HTML/CSS/SASS",
+  "Node",
+  "MongoDB",
+  "Material UI",
+  "AWS",
+  "Scrum",
+];
